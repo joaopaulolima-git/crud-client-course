7e7d13194aaffe2a29c94354b89946f16ffa80c7
João Paulo <jplopapa.git@gmail.com>
2022-03-03 14:43:54 -0300

    H2 Database

--- backend/src/main/java/com/jpdev/crudclient/resources/ClientResource.java
+++ backend/src/main/java/com/jpdev/crudclient/resources/ClientResource.java
@@ -1,25 +1,26 @@
 package com.jpdev.crudclient.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jpdev.crudclient.entities.Client;
+import com.jpdev.crudclient.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public ResponseEntity <List<Client>> findAll(){
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "João","123456789", 200.00 , null , 2 ));
-		list.add(new Client(2L, "Bob","987654321", 200.00 , null , 3 ));
-		list.add(new Client(3L, "Maria","1111222333", 200.00 , null , 2 ));
+		List<Client> list = service.findAll();		
 		return ResponseEntity.ok().body(list);
 		
 	}
