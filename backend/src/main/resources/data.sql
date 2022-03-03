bef937be54b86d57881a7e0f77dc98cf6c774c3d
João Paulo <jplopapa.git@gmail.com>
2022-03-03 17:15:51 -0300

    Database seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_client (name, cpf, income, children) VALUES ('Joao Paulo', '123456', 2000.0, 2);
+INSERT INTO tb_client (name, cpf, income, children) VALUES ('Paula ', '654321', 3000.0, 1);
+INSERT INTO tb_client (name, cpf, income, children) VALUES ('Rafael', '125463', 2500.0, 0);
+INSERT INTO tb_client (name, cpf, income, children) VALUES ('Dainel', '548796', 4000.0, 3);
